73ccc98c67f283c7ca1065652352a26ca7dd438e
rohit4825 <supervikalpa@gmail.com>
2024-04-12 13:50:39 +0530

    updated

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Rohit</h1>
+        <p className={styles.description}>
+          I'm a full-stack developer with 2 years of experience using MERN stack and Full Stack Development. Reach out if you'd like to learn more!
+        </p>
+        <a href="mailto:rohit4826iiitu@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
